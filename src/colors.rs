cee67ed201b9ab705bd442888319bf30d081949a
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-06-08 11:55:24 +1000

    write tests for colors

--- src/colors.rs
+++ src/colors.rs
@@ -53,3 +53,102 @@ impl Color {
             + (decimal.2 - other_decimal.2).powf(2.0)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_color_distance() {
+        let black = Color::new(0, 0, 0);
+        let red = Color::new(255, 34, 10);
+
+        let dark_blue = Color::new(40, 40, 100);
+
+        assert!(dark_blue.distance(&black) < dark_blue.distance(&red));
+    }
+
+    #[test]
+    fn test_color_convert_to_decimal() {
+        let rgb = (128, 50, 43);
+        let decimal_rgb = (
+            rgb.0 as f64 / 255.0,
+            rgb.1 as f64 / 255.0,
+            rgb.2 as f64 / 255.0,
+        );
+
+        let color = Color::new_from_decimal_tuple(decimal_rgb);
+
+        assert_eq!(decimal_rgb, color.convert_to_decimal());
+    }
+
+    #[test]
+    fn test_color_new_from_decimal() {
+        let rgb = (128, 50, 43);
+        let decimal_rgb = (
+            rgb.0 as f64 / 255.0,
+            rgb.1 as f64 / 255.0,
+            rgb.2 as f64 / 255.0,
+        );
+
+        let color = Color::new_from_decimal(decimal_rgb.0, decimal_rgb.1, decimal_rgb.2);
+
+        assert_eq!(color.red, rgb.0);
+        assert_eq!(color.green, rgb.1);
+        assert_eq!(color.blue, rgb.2);
+    }
+
+    #[test]
+    fn test_color_new_from_decimal_tuple() {
+        let rgb = (128, 50, 43);
+        let decimal_rgb = (
+            rgb.0 as f64 / 255.0,
+            rgb.1 as f64 / 255.0,
+            rgb.2 as f64 / 255.0,
+        );
+
+        let color = Color::new_from_decimal_tuple(decimal_rgb);
+
+        assert_eq!(color.red, rgb.0);
+        assert_eq!(color.green, rgb.1);
+        assert_eq!(color.blue, rgb.2);
+    }
+
+    #[test]
+    fn test_color_new_from_tuple() {
+        let rgb = (255, 12, 65);
+        let color = Color::new_from_tuple(rgb);
+
+        assert_eq!(rgb.0, color.red);
+        assert_eq!(rgb.1, color.green);
+        assert_eq!(rgb.2, color.blue);
+    }
+
+    #[test]
+    fn test_color_new() {
+        let rgb = (255, 12, 65);
+        let color = Color::new(rgb.0, rgb.1, rgb.2);
+
+        assert_eq!(rgb.0, color.red);
+        assert_eq!(rgb.1, color.green);
+        assert_eq!(rgb.2, color.blue);
+    }
+
+    #[test]
+    fn test_decimal_to_u8() {
+        let rgb = (128, 50, 43);
+        let decimal_rgb = (
+            rgb.0 as f64 / 255.0,
+            rgb.1 as f64 / 255.0,
+            rgb.2 as f64 / 255.0,
+        );
+
+        let deciaml_to_rgb = (
+            decimal_to_u8(decimal_rgb.0),
+            decimal_to_u8(decimal_rgb.1),
+            decimal_to_u8(decimal_rgb.2),
+        );
+
+        assert_eq!(rgb, deciaml_to_rgb);
+    }
+}
